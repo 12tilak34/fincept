60ca4201dc7f95b13c97ef12c7a3392b69367ca8
12tilak34 <mygoalisfix@gmail.com>
2022-07-03 10:54:29 +0530

    done

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "finceptcb6d97cb": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ android/app/src/main/kotlin/com/fincept/new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fincept.new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
